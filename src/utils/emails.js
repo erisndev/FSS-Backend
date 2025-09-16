301049546492e3e6d1d76524d2c9827da391dd41
erisndev <dev@erisngraduate.com>
2025-09-16 10:51:44 +0200

    BackEnd 16/09

--- src/utils/emails.js
+++ src/utils/emails.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 // ------------------- SMTP Setup -------------------
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT) || 587,
   secure: false, // true for 465, false for others
